e5064d260636db6229f28b72c0a37b4e58f4e5e0
geromt <gerardomartinez205@gmail.com>
2024-07-07 21:35:04 -0600

    Agregar archivos de proyecto.

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace NumberGuessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Number Guessing");
+            Random random = new Random();
+            int winnerNumber = random.Next(0, 101);
+            int guessedNumber;
+            int triesCount = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Guess a number:");
+                var inputNumber = Console.ReadLine();
+
+                if (!int.TryParse(inputNumber, out guessedNumber))
+                {
+                    Console.WriteLine("Input an integer number, please");
+                    continue;
+                }
+
+                if (guessedNumber > winnerNumber)
+                    Console.WriteLine("To High. Try with a lower one...");
+                else if (guessedNumber < winnerNumber)
+                    Console.WriteLine("To Low. Try with a higher one...");
+                else
+                    break;
+
+                triesCount++;
+            }
+
+            Console.WriteLine("Congratulations! You've guess the right number");
+            Console.WriteLine(string.Format("You only take {0} tries", triesCount));
+
+            Console.ReadLine();
+        }
+    }
+}
